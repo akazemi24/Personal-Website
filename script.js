5faa3c8891a3a2bcbf6636ffe9ad4b5afc9aa7a5
Armita Kazemi <ak5958@princeton.edu>
2026-08-07 23:15:29 -0500

    make the layout look beter and add sections

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const updates = [
-    "Currently learning: AWS deployment",
-    "Currently building: AI-powered applications",
-    "Interested in: machine learning and product design",
-    "Working on: recommendation systems and real-world AI tools"
-  ];
-  
-  let index = 0;
-  
-  function changeText() {
-    document.getElementById("changing-text").textContent = updates[index];
-    index = (index + 1) % updates.length;
-  }
+// No interactivity yet.
